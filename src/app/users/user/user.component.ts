80a9937e1ee6c0914b66f453aafe26adc6e6cff6
alpersarac <alper.sarac42@gmail.com>
2023-06-17 18:39:30 +0300

    subscribing and ubsubscribing for an observable.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit,OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route:ActivatedRoute) { }
 
@@ -16,13 +19,16 @@ export class UserComponent implements OnInit {
       id:this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name'],
     };
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params:Params)=>{
           this.user.id=params['id'];
           this.user.name=params['name'];
         }
       )
+  }
+  ngOnDestroy(): void {
+
   }
 
 }
