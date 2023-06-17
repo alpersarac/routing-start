c96acfd7bd08338dc9314f8a558e053bbdb28298
alpersarac <alper.sarac42@gmail.com>
2023-06-17 23:05:42 +0300

    passing params programmatically.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ const appRoutes: Routes =[
   { path: 'users', component: UserComponent},
   { path: 'users/:id/:name', component: UserComponent},
   { path: 'servers', component: ServersComponent},
-  { path: 'servers/:id/edit', component: ServersComponent},
+  { path: 'servers/:id/edit', component: EditServerComponent},
 ];
 @NgModule({
   declarations: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,8 +12,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-  onLoadServers(){
-    this.router.navigate(['/servers']);
+  onLoadServer(id: number){
+    this.router.navigate(['/servers',id, 'edit'],{queryParams:{allowEdit:'1'},fragment:'loading'});
   }
 
 }
