67d4cb85a0978bce58f67e98c71c3dbab17ede5e
alpersarac <alper.sarac42@gmail.com>
2023-06-17 17:23:56 +0300

    Relative path onClick

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,14 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router:Router,
+              private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
-
+  onReloadServers(){
+    //this.router.navigate(['/servers'],{ relativeTo:this.route })
+  }
 }
